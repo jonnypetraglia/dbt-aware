3d97b30dc45d5ba25580962db4e7b8b9dadb32f0
notbryant <notbryant@gmail.com>
2018-08-25 16:06:51 -0600

    Added skills

--- app/src/main/java/me/beingpresent/dbtaware/EntryActivity.kt
+++ app/src/main/java/me/beingpresent/dbtaware/EntryActivity.kt
@@ -3,7 +3,8 @@ package me.beingpresent.dbtaware
 import android.app.Activity
 import android.os.Bundle
 import android.util.TypedValue
-import android.view.View
+import android.view.Gravity
+import android.widget.CheckBox
 import android.widget.LinearLayout
 import android.widget.RatingBar
 import android.widget.TextView
@@ -13,6 +14,36 @@ class EntryActivity : Activity() {
     private val emotions = arrayOf("Sadness", "Anger", "Anxiety", "Shame", "Happiness")
     private val urges = arrayOf("Self-Harm", "To kill myself")
     private val actions = arrayOf("Self-Harm", "Validate Myself")
+    private val mindfulnessSkills = arrayOf(
+            "Wise Mind",
+            "Observe: Just notice without reacting",
+            "Describe: Put words on, label emotion",
+            "Participate: Enter into the experience",
+            "Nonjudgmental Stance",
+            "One-Mindfully In The Moment",
+            "Effectiveness: Focus on what works"
+    )
+    private val interpersonalSkills = arrayOf(
+            "Clarified Priorities",
+            "Objective Effectiveness (DEAR MAN)",
+            "Relationship Effectiveness (GIVE)",
+            "Self-respect Effectiveness (FAST)"
+    )
+    private val emotionRegulationSkills = arrayOf(
+            "Reduced Vulnerability (PLEASE)",
+            "Built MASTERy",
+            "Built Positive Experiences",
+            "Opposite Action"
+    )
+    private val distressToleranceSkills = arrayOf(
+            "Distract (Wise Mind ACCEPTS)",
+            "Self-soothe",
+            "IMPROVE the Moment",
+            "Pros and Cons",
+            "TIP-temperature, intense exercise, progressive/paired relation",
+            "Radical Acceptance"
+    )
+
     private lateinit var entryContents: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,35 +51,45 @@ class EntryActivity : Activity() {
         setContentView(R.layout.activity_entry)
         entryContents = this.findViewById<LinearLayout>(R.id.entryContents)
 
-        var txt = TextView(this)
-        txt.text = "Emotions"
-        txt.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32F)
-        entryContents.addView(txt)
+        createHeader("Emotions")
         for(emotion in emotions) {
-            createRow(emotion)
+            createRating(emotion)
         }
 
-        txt = TextView(this)
-        txt.text = "Urges"
-        txt.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32F)
-        entryContents.addView(txt)
+        createHeader("Urges")
         for(urge in urges) {
-            createRow(urge)
+            createRating(urge)
         }
 
-        txt = TextView(this)
-        txt.text = "Actions"
-        txt.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32F)
-        entryContents.addView(txt)
+        createHeader("Actions")
         for(action in actions) {
-            createRow(action)
+            createRating(action)
         }
 
 
+        createHeader("Mindfulness Skills")
+        for(skill in mindfulnessSkills)
+            createSkill(skill)
+        createHeader("Interpersonal Skills")
+        for(skill in interpersonalSkills)
+            createSkill(skill)
+        createHeader("Emotional Regulation Skills")
+        for(skill in emotionRegulationSkills)
+            createSkill(skill)
+        createHeader("Distress Tolerance Skills")
+        for(skill in distressToleranceSkills)
+            createSkill(skill)
     }
 
-    fun createRow(str: String) {
-        var txt = TextView(this)
+    fun createHeader(str: String) {
+        val txt = TextView(this)
+        txt.text = str
+        txt.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32F)
+        entryContents.addView(txt)
+    }
+
+    fun createRating(str: String) {
+        val txt = TextView(this)
         txt.text = str
         txt.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F)
 
@@ -56,12 +97,22 @@ class EntryActivity : Activity() {
         bar.numStars = 5
         bar.stepSize = 1.0F
 
-        entryContents.addView(txt)
-        entryContents.addView(bar)
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.HORIZONTAL
+        layout.addView(txt)
+        layout.addView(bar)
+        (txt.layoutParams as LinearLayout.LayoutParams).weight = 1.0F
+        (txt.layoutParams as LinearLayout.LayoutParams).gravity = Gravity.CENTER
+        entryContents.addView(layout)
 
-        txt.layoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT
-        txt.gravity = View.TEXT_ALIGNMENT_CENTER
+//        bar.layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+    }
 
-        bar.layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+
+    fun createSkill(str: String) {
+        val checkBox = CheckBox(this)
+        checkBox.setText(str)
+        checkBox.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F)
+        entryContents.addView(checkBox)
     }
 }
